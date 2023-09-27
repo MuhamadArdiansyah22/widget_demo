12b8f63ed1a4bbbd425c14434acdebd4d1ca6cdb
Muhamad Ardiansyah <ardianstar02@gmail.com>
2023-09-27 17:53:26 +0700

    Add text left

--- lib/main.dart
+++ lib/main.dart
@@ -11,69 +11,119 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-              title: const Text('Flutter Demo'),
-              backgroundColor: Color.fromARGB(255, 195, 0, 255)),
-          body: const Padding(
-            padding: EdgeInsets.fromLTRB(20, 40, 60, 0),
-            child: Column(children: [
-              Text(
-                'Biodata',
-                maxLines: 2,
-                textAlign: TextAlign.left,
-                style: TextStyle(
+        appBar: AppBar(
+          title: const Text('Aplikasi Flutter Sederhana'),
+          backgroundColor: const Color.fromARGB(255, 195, 0, 255),
+        ),
+        body: const Padding(
+          padding: EdgeInsets.fromLTRB(20, 40, 60, 0),
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'Biodata',
+                  maxLines: 2,
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
                     fontSize: 15,
                     fontStyle: FontStyle.italic,
-                    color: Colors.lightBlue),
+                    color: Colors.lightBlue,
+                  ),
+                ),
               ),
-              Text('Atomic Habbits',
-                  overflow: TextOverflow.clip,
-                  style: TextStyle(
-                    fontSize: 10,
-                    fontWeight: FontWeight.bold,
-                  )),
+              Text(
+                'Atomic Habits',
+                overflow: TextOverflow.clip,
+                style: TextStyle(
+                  fontSize: 10,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
               SizedBox(
                 height: 10,
               ),
-              Text.rich(TextSpan(children: <TextSpan>[
-                TextSpan(
-                    text: 'Nama ',
-                    style: TextStyle(fontWeight: FontWeight.bold)),
-                TextSpan(
-                  text: ' : Muhamad Ardiansyah',
-                )
-              ])),
-              Text.rich(TextSpan(children: <TextSpan>[
-                TextSpan(
-                    text: 'Kelahiran (TTL) ',
-                    style: TextStyle(fontWeight: FontWeight.bold)),
-                TextSpan(
-                  text: ' : Bandung, 22 Juni 2004',
-                )
-              ])),
-              Text.rich(TextSpan(children: <TextSpan>[
-                TextSpan(
-                    text: 'Alamat ',
-                    style: TextStyle(fontWeight: FontWeight.bold)),
-                TextSpan(
-                  text: ' : Jl, Sadang Legok Kidul 4 No.7',
-                )
-              ])),
-              Text.rich(TextSpan(children: <TextSpan>[
-                TextSpan(
-                    text: 'Hobi ',
-                    
-                    style: TextStyle(fontWeight: FontWeight.bold,)),
-                TextSpan(
-                  text: ' : Main',
-                )
-              ])),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text.rich(
+                  TextSpan(
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: 'Nama ',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      TextSpan(
+                        text: ' : Muhamad Ardiansyah',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text.rich(
+                  TextSpan(
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: 'Kelahiran (TTL) ',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      TextSpan(
+                        text: ' : Bandung, 22 Juni 2004',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text.rich(
+                  TextSpan(
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: 'Alamat ',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      TextSpan(
+                        text: ' : Jl, Sadang Legok Kidul 4 No.7',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text.rich(
+                  TextSpan(
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: 'Hobi ',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      TextSpan(
+                        text: ' : Bermain dan Belajar sesuatu yang baru',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
               Text(
-                  'lorem, ipsum dolor sit amet consectetur suscipit nobis, aliquam, non consectetur. Magnam.',
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(fontSize: 15)),
-            ]),
-          )),
+                'See you the next application',
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.lightBlue,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
